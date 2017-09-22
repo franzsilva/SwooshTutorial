71e6b9e4190d5b6a24e8de22df21ec3445fdb144
franzsilva <franzsilva@gmail.com>
2017-09-22 17:59:03 -0500

    Adding new functionality

--- app/src/main/java/com/franzsilva/swoosh/WelcomeActivity.kt
+++ app/src/main/java/com/franzsilva/swoosh/WelcomeActivity.kt
@@ -2,11 +2,27 @@ package com.franzsilva.swoosh
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_welcome.*
+import android.widget.Toast
+
+
 
 class WelcomeActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_welcome)
+
+
+        button.setOnClickListener {
+            val context = applicationContext
+            val text = "Hello toast!"
+            val duration = Toast.LENGTH_SHORT
+
+            val toast = Toast.makeText(context, text, duration)
+            toast.show()
+        }
+
     }
+
 }
